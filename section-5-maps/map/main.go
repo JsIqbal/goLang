bec9ea5b3e91f8e31959f64dd2a68436c953840c
sharetripiqbal <mdiqbal@sharetrip.net>
2024-10-21 09:51:01 +0600

    Initialize Section-5 Maps

---
+++ section-5-maps/map/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	colors := map[string]string{ // map is a structure in go that takes the keys of same type and the values of same type.
+		"red":   "#ff0000",
+		"green": "#fgh4344",
+	}
+
+	fmt.Println(colors)
+}
