72df7555b6817c9a1946fa778e26301b1cae643f
kraken unleashed <javascriptiqbal@gmail.com>
2025-02-02 10:56:17 +0000

    Being Smart With GoRoutines

--- section-7-channels-go-routines/main.go
+++ section-7-channels-go-routines/main.go
@@ -1,3 +1,41 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"net/http"
+// )
+
+// func main() {
+// 	links := []string{
+// 		"http://google.com",
+// 		"http://youtube.com",
+// 		"http://facebook.com",
+// 	}
+
+// 	c := make(chan string)
+
+// 	for _, link := range links {
+// 		go checkLink(link, c)
+// 	}
+
+// 	fmt.Println(<- c)
+// 	fmt.Println(<- c)
+// 	fmt.Println(<- c)
+// }
+
+// func checkLink(link string, c chan string) {
+// 	_, err := http.Get(link)
+// 	if err != nil {
+// 		fmt.Println(link, " Might Be Down!")
+// 		c <- "might be down from channel!"
+// 		return 
+// 	}
+
+// 	fmt.Println(link, " Is Up!")
+// 	c <- "is up!"
+// }
+
+
 package main
 
 import (
@@ -18,9 +56,9 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	fmt.Println(<- c)
-	fmt.Println(<- c)
-	fmt.Println(<- c)
+	for i := 0; i < len(links); i ++ {
+		fmt.Println(<-c)
+	}
 }
 
 func checkLink(link string, c chan string) {
